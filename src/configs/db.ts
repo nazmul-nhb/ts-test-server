c9ec65ee142670f9b306ab6ae2738e7940417d9b
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-09-23 16:58:57 +0600

    replaced console.log with warn

--- src/configs/db.ts
+++ src/configs/db.ts
@@ -30,7 +30,7 @@ export const connectDB = async () => {
 
 		// Optional: Listen for disconnection
 		mongoose.connection.on('disconnected', () => {
-			console.log('⚠️	MongoDB is Disconnected!');
+			console.warn('⚠️	MongoDB is Disconnected!');
 		});
 	} catch (error) {
 		if (error instanceof Error) {
@@ -38,7 +38,7 @@ export const connectDB = async () => {
 		} else {
 			console.error('⚠️	Unknown Error Occurred!');
 		}
-		console.log('⚠️	MongoDB is Not Connected!');
+		console.warn('⚠️	MongoDB is Not Connected!');
 		process.exit(1);
 	}
 };
